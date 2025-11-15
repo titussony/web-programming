b1027734f0f2c1cf1e6b170a440c9b535a39b712
Titus sony <tituphlip2018@gmail.com>
2025-11-15 15:47:06 +0530

    Add files via upload

---
+++ aray.php
@@ -0,0 +1,10 @@
+<?php
+$a=array("anjala","christeena","blessy","angela");
+print_r($a);
+echo("<br>ascending order:");
+asort($a);
+print_r($a);
+echo("<br>Descending order : ");
+asort($a);
+print_r($a);
+?>
